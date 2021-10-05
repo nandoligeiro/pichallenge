15212bc9ffb2fceab6d05a7dc34a021ad78bb2eb
nandoligeiro <Ligeiro23#>
2021-10-05 10:02:23 -0300

    WIP: Implementado estrutura

--- app/src/main/java/com/picpay/desafio/android/data/UserRepository.kt
+++ app/src/main/java/com/picpay/desafio/android/data/UserRepository.kt
@@ -1,10 +1,8 @@
 package com.picpay.desafio.android.data
 
-
 import com.picpay.desafio.android.domain.model.User
-import javax.inject.Inject
 
-class UserRepository @Inject constructor(private val dataSource: UserDataSource) {
+class UserRepository (private val dataSource: UserDataSource) {
 
     suspend fun getUsers(): List<User> = dataSource.getUsers()
 
